d5fe6d0c32362b467ac23cc195167bb37078cc90
AnjuMKaimal <anjuanand.252014@gmail.com>
2016-03-27 11:52:35 +0530

    Coursera Week 3 Programing Assignment
    This work contains the assignment of R programing given by John Hopkins
    University.
    Topic -- To find the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function makes a list of of methods with get and set values and inversing matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
+     inVse1 <- NULL #intialises the inverse
+
+
+        set <- function(userMatrix) 
+        {
+                x <<- userMatrix
+                inVse1 <<- NULL
+        }
+        
+        get <- function() x 
+
+        setINverse <- function(inVerse) 
+         {
+           inVse1 <<- inVerse
+           return(inVse1)
+          }
+        getINverse <- function() inVse1 
+        list(set = set, get = get,
+             setINverse= setINverse,
+             getINverse = getINverse )
+
 
 }
 
 
-## Write a short comment describing this function
+##cacheSolve tries to find the the inverse, if its written in first function otherwise it calculates inverse itself and dispays the solution
+
 
 cacheSolve <- function(x, ...) {
+
         ## Return a matrix that is the inverse of 'x'
+         inv <- x$getINverse ()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setINverse(inv)
+        inv
+  
 }
