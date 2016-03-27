495983bf5904e11c7ead2108f3f07209dd2a91bc
AnjuMKaimal <anjuanand.252014@gmail.com>
2016-03-27 12:54:25 +0530

    Week3 Programming Assignment-Lexical Scoping
    Invertible Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -41,5 +41,23 @@ cacheSolve <- function(x, ...) {
         inv <- solve(data, ...)
         x$setINverse(inv)
         inv
-  
 }
+#Sample Output
+#############################################
+#> my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+#> my_matrix$get()
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> my_matrix$getINverse()
+#NULL
+#> cacheSolve(my_matrix)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cacheSolve(my_matrix)
+#getting cached data
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+################################################
